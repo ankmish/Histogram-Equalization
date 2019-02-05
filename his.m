b4f26f468531745f23a548466bb4b1a951b73ae6
Ankit Mishra <33161970+cef1998@users.noreply.github.com>
2019-02-05 22:40:08 +0530

    Update his.m

--- his.m
+++ his.m
@@ -2,12 +2,7 @@ tmp = imread('download.jpg');
 I = tmp;
 % If the image is a color image, change it to a gray image
 [height,width, colormap]=size(tmp);
-%if (colormap > 1)
- % disp(colormap);
-  I = rgb2gray(tmp);
- % I = tmp[:,:,1];
- % imsave
-% end
+I = rgb2gray(tmp);
 
 % show the original image
 subplot(241)
@@ -18,10 +13,10 @@ title('Original Image');
 no_of_pixels = com_hist(I,'Histogram of Original Image',2);
 
 % prob_of_pixel is the probability of an occurrence of each gray level
-prob_of_pixel = com_normalized_hist(no_of_pixels,I,'Normalized Histogram of Original Image',3);
+prob_of_pixel = com_normalized_hist(no_of_pixels,I);
 
 % cum_pixel is the cumulative distribution function
-cum_pixel = comp_cumul_hist(prob_of_pixel,'Cumulative Histogram of Original Image',4);
+cum_pixel = comp_cumul_hist(prob_of_pixel);
 
 % cum_pixel convert to new gray levels
 mapping = zeros(1,256);
@@ -39,13 +34,9 @@ imshow(I)
 title('New Image');
 %imsave
 
-
-
 function f = com_hist(I,title1,position)
 %compute_histogram 
-%   I: the image
-%   title1: histogram's title
-%   position: histogram's position
+
 [height,width]=size(I);
 % Compute the number of occurrences of each gray level
 no_of_pixels = zeros(1,256);                      % saare pixels ke occurance nikal lo
@@ -61,13 +52,7 @@ title(title1);
 f = no_of_pixels;
 end
 
-
-function f = com_normalized_hist(no_of_pixels,I,title1,position)
-%compute_normalized_histogram
-%   no_of_pixels: number of each gray level
-%   I: the image
-%   title1: histogram's title
-%   position: histogram's position
+function f = com_normalized_hist(no_of_pixels,I)
 
 [height,width]=size(I);
 
@@ -77,18 +62,10 @@ for i = 1:256
     prob_of_pixel(i) = no_of_pixels(i) / (height * width * 1.0);
 end
 
-%subplot(2,4,position)
-%bar(prob_of_pixel)
-%title(title1);
-
 f = prob_of_pixel;
 end
 
-function f = comp_cumul_hist(prob_of_pixel,title1,position)
-%compute_cumulative_histogram
-%   prob_of_pixel: probability density of each gray level
-%   title1: histogram's title
-%   position: histogram's position
+function f = comp_cumul_hist(prob_of_pixel)
 
 % Compute the cumulative distribution function
 cum_pixel = zeros(1,256);
@@ -99,10 +76,6 @@ for i = 1:256
         cum_pixel(i) = cum_pixel(i-1) + prob_of_pixel(i);
     end
 end
-
-%subplot(2,4,position);
-%bar(cum_pixel)
-%title(title1);
 
 f = cum_pixel;
 end
